93146eecbe4aa37c00ba2a09b390f5e99f10d4fb
Manami Kokado <kokado313.mnm@gmail.com>
2024-09-15 23:36:24 +0900

    Nextjs勉強 20240915_2

---
+++ app/next-sample/pages/sayHello.tsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function SayHello() {
+  const [data, setData] = useState({name: ''})
+
+  useEffect(() => {
+    fetch('api/hello')
+      .then((res) => res.json())
+      .then((profile) => {
+        setData(profile)
+      })
+  }, [])
+  return <div>hello {data.name}</div>
+}
+
+export default SayHello
